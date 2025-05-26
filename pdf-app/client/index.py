9c82ee2e3d5b6a1b9ba334e06e4cd373199e565e
Arturo Parrales <arturo.ps14@gmail.com>
2025-05-25 20:22:16 -0400

    Fixed the state of model selection box in UI

--- pdf-app/client/index.py
+++ pdf-app/client/index.py
@@ -73,18 +73,18 @@ def main():
 	"""Run the client"""
 	st.header("🦮 :blue[Tame Your PDF] 🙂🤖", divider="blue", anchor=False)
 
-	# Init session state
+	# Init session state (only once)
 	init_states_dict = {
 		"messages": [],
 		"pdf_pages": [],
 		"vector_db_ready": False,
 		"pdf_process_button_disabled": True,
-		"is_model_selected": False,
+		"model_selected": None,
 	}
 	for key, default_value in init_states_dict.items():
 		if key not in st.session_state:
 			st.session_state[key] = default_value
-	logger.info("Session state initialized")
+			logger.info(f"Session state: '{key}: {default_value}' initialized")
 
 	# Upload file
 	st.subheader("1️⃣ Upload PDF")
@@ -118,7 +118,7 @@ def main():
 		disabled=st.session_state["pdf_process_button_disabled"],
 	)
 
-	# logger.info("File uploaded")
+	logger.info("File uploaded for preview")
 	col1.subheader("PDF Preview")
 	if file_uploaded:
 		with st.spinner(":green[Generating PDF preview ...]"):
@@ -141,6 +141,9 @@ def main():
 	if st.session_state.get("pdf_pages"):
 		response = requests.get(f"{BACKEND_URL}/list/models")
 		models = response.json()["models"]
+		# ensure the order of the models for consistency
+		# and to allow us to use indexing to maintian state on UI
+		models.sort()
 
 	if pdf_proccess_button:
 		logger.info("File submited to create vector database")
@@ -170,12 +173,27 @@ def main():
 	# Chat interface
 	with col2:
 		col2.subheader("3️⃣ Pick a Model")
-		selected_model = col2.selectbox(
+
+		# recover the index of the current selected model
+		# so that we can maintain the state of the selectbox
+		selected_model_index = (
+			models.index(st.session_state["model_selected"])
+			if st.session_state["model_selected"]
+			else None
+		)
+
+		col2.selectbox(
 			"3️⃣ Pick a Model",
 			models,
+			index=selected_model_index,
 			key="model_select",
 			label_visibility="collapsed",
+			# use the selected value to change session var state
+			on_change=lambda: st.session_state.update(
+				{"model_selected": st.session_state["model_select"]}
+			),
 		)
+
 		col2.subheader("Chat")
 		message_container = st.container(height=400, border=True)
 
@@ -206,7 +224,10 @@ def main():
 							try:
 								response = requests.post(
 									f"{BACKEND_URL}/question",
-									json={"prompt": prompt, "model": selected_model},
+									json={
+										"prompt": prompt,
+										"model": st.session_state["model_selected"],
+									},
 								)
 								response.raise_for_status()
 								response_json = response.json()
@@ -214,7 +235,7 @@ def main():
 								st.markdown(response_data)
 							except requests.exceptions.HTTPError as e:
 								logger.error(f"HTTP error: {e}")
-								if selected_model is None:
+								if st.session_state["model_selected"] is None:
 									st.error(
 										"😵 Error processing the request. "
 										"Don't forget to select a model."
